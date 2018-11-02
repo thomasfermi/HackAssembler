002f4298e084294794f190bba1ae45db232276cf
thomasfermi <mario.theers@gmail.com>
2018-11-02 09:18:12 +0100

    Added CommandType enum and simple function to determine the CommandType.

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,15 @@
 
 use std::str::Lines;
 
+#[derive(Debug)]
+pub enum CommandType {
+    //TODO: attach data to different types, such as address for A, (dest,comp,jmp) for C and symbol for L
+    A_command,
+    C_command,
+    L_command,
+    invalid_command,
+}
+
 pub struct Parser<'a> {
     input_iterator : Lines<'a>,
     current_command : Option<String>,
@@ -32,10 +41,35 @@ impl<'a>  Parser<'a> {
         }
     }
 
+    pub fn command_type(&self) -> CommandType {
+        // TODO: more thorough checking for invalid commands
+        let c = self.current_command.as_ref().unwrap();
+        let find_at = c.find("@");
+        let find_equal = c.find("=");
+        let find_semicolon = c.find(";");
+        let find_paranthesis_open = c.find("(");
+        let find_paranthesis_closed = c.find(")");
+
+        if find_at!= None && find_equal == None && find_semicolon == None {
+            return CommandType::A_command;
+        }
+        else if find_at == None && (find_equal!=None || find_semicolon != None) {
+            return CommandType::C_command;
+        }
+        else if find_paranthesis_open != None && find_paranthesis_closed != None {
+            return CommandType::L_command;
+        }
+        else {
+            return CommandType::invalid_command; //TODO: This is an error and should be handled!
+        }
+
+    }
+
     pub fn silly_print(&mut self){
         self.advance();
         while self.current_command != None {          
-            println!("{}", self.current_command.as_ref().unwrap());            
+            println!("{}", self.current_command.as_ref().unwrap());  
+            println!("{:?}", self.command_type());          
             self.advance();
         }
     }
